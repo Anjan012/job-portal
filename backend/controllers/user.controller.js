f8e0ea1262ba13caabfc9a62e106d832f1f3c6aa
Anjan012 <anjankhadka199@gmail.com>
2026-01-19 11:18:54 +0545

    wrote controller logic for register, login and logout

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,138 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+  try {
+    const { fullname, email, phoneNumber, password, role } = req.body; // destructuring from req.body
+
+    // validation
+    if (!!fullname || !email || phoneNumber || !password || !role) {
+      return res.status(400).json({
+        message: "Something is missing in the input fields",
+        success: false,
+      });
+    }
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        message: "User already exists with this email.",
+        success: false,
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.create({
+      fullname,
+      email,
+      phoneNumber,
+      password: hashedPassword,
+      role,
+    });
+
+    return res.status(201).json({
+      message: "User registered successfully.",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password, role } = req.body;
+
+    // validation
+    if (!email || !password || !role) {
+      return res.status(400).json({
+        message: "Something is missing in the input fields",
+        success: false,
+      });
+    }
+
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Incorrect email or password.",
+        success: false,
+      });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        message: "Incorrect email or password.",
+        success: false,
+      });
+    }
+
+    // check for role
+    if (role !== user.role) {
+      return res.status(400).json({
+        message: "Account does not exist with current role.",
+        success: false,
+      });
+    }
+
+    const tokenData = {
+      userId: user._id,
+    };
+
+    // generate token
+    const token = await jwt.sign(tokenData, process.env.SECRET_KEY, {
+      expiresIn: "1d",
+    });
+
+    user = {
+      // ...user._doc // to get all the fields from user object
+      // but we don't want to send password field to frontend
+
+      _id: user._id,
+      fullname: user.fullname,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      role: user.role,
+      profile: user.profile,
+    };
+    // storing token in cookie
+    return res
+      .status(200)
+      .cookie("token", token, {
+        maxAge: 24 * 60 * 60 * 1000, // 1 day
+        httpOnly: true, // JavaScript (document.cookie) cannot read this cookie → protects against XSS attacks
+        sameSite: "strict", // CSRF protection
+      })
+      .json({
+        message: `Welcome back, ${user.fullname}`,
+        user,
+        success: true,
+      });
+  } catch (error) {
+    console.log("Error in login controller:", error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+      success: false,
+    });
+  }
+};
+
+export const logout = async (req, res) => {
+
+    try {
+
+        return res.status(200).cookie("token", "", {
+            maxAge:0
+        }).json({
+            message: "Logged out successfully",
+            success:true
+        });
+        
+    } catch (error) {
+        console.log("Error in logout controller:", error);
+    }
+}
