db192f3433dc05b71ccb0a5213da33c755d45da8
Anjan012 <anjankhadka199@gmail.com>
2026-01-25 11:46:07 +0545

    Application controller logic completed

--- backend/controllers/application.controller.js
+++ backend/controllers/application.controller.js
@@ -95,8 +95,67 @@ export const getAppliedJobs = async (req, res) => {
 // admin will see how many applications are there for their job postings
 const getApplicants = async (req, res) => {
     try {
+
+      const {jobId} = req.params;
+      const job = await Job.findById(jobId).populate({
+        path:'applications',
+        options: {sort:{createdAt:-1}},
+        populate: {
+          path:'applicant',
+          options: {sort:{createdAt: -1}}
+        }
+      });
+
+      if(!job) {
+        return res.status(404).json({
+          message: "Job not found!",
+          success: false
+        });
+      };
+
+      return res.status(200).json({
+        job,
+        success: true
+      });
         
     } catch (error) {
         console.log(error);
     }
 };
+
+
+export const updateStatus = async (req, res) => {
+  try {
+    const status = req.body;
+    const applicationId = req.params.id;
+
+    if(!status) {
+      return res.status(400).json({
+          message: "Status is required!",
+          success: false
+        });
+    };
+
+    //find the application by applicantion id
+    const application = await Application.findOne({_id:applicationId});
+
+    if(!application) {
+      return res.status(404).json({
+          message: "Application not found!",
+          success: false
+        });
+    };
+
+    // update the status 
+    application.status = status.toLowerCase();
+    await application.save();
+
+    return res.status(200).json({
+      message: "Status updated successfully",
+      success:true
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+}
